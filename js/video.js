ab1147b59e6877f0ce6f2750d36cb9c1f31c45c1
emmiewells <70598315+emmiewells@users.noreply.github.com>
2020-11-16 17:51:25 -0500

    update

--- js/video.js
+++ js/video.js
@@ -1,14 +1,51 @@
-var video;
+var video = document.querySelector('video');
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+	video.play();
+	span = document.getElementById("volume").innerHTML = (video.volume * 100) + "%";
+
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
 });
 
+document.querySelector('#volumeSlider').addEventListener("change", function() {
+	video.volume = this.value/100;
+	span = document.getElementById("volume").innerHTML = (video.volume * 100) + "%";
+
+});
+
+document.querySelector('#mute').addEventListener("click", function() {
+	if (video.muted == true) {
+			video.muted = false;
+			document.getElementById("mute").innerHTML = "Mute";
+	}
+	else {
+			video.muted = true;
+			document.getElementById("mute").innerHTML = "Unmute" ;
+	}
+
+});
+
+document.querySelector('#slower').addEventListener("click", function() {
+		video.playbackRate = video.playbackRate * .9;
+		console.log(video.playbackRate);
+});
+
+document.querySelector('#faster').addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.1;
+	console.log(video.playbackRate);
+
+});
+
+document.querySelector('#skip').addEventListener("click", function() {
+	video.currentTime = (video.currentTime + 5);
+	video.play();
+	console.log(video.currentTime);
+	
+});
